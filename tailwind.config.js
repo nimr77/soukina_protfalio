5f33e8c02faa88918aebf3e96221313c1a1a37eb
nimr77 <nimr.h.sawafta@gmail.com>
2025-03-22 03:12:27 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
       colors: {
         primary: "#DC3544", // Define your primary color
 
