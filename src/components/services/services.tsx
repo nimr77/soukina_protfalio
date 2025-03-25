60a747d4aad30eb51a6ff5ea6c395e8af06e5c34
nimr77 <nimr.h.sawafta@gmail.com>
2025-03-25 00:07:05 +0000

    update

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import React from 'react';
-import ServiceCard from './ServiceCard';
+import ServiceCard from './service';
 
 interface Service {
   id: string;
